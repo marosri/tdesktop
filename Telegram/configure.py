513f9b17dac7eecaaec3e20696e459d706aa6ca6
marosri <57309542+marosri@users.noreply.github.com>
2025-11-25 15:25:18 +0800

    Update configure.py

--- Telegram/configure.py
+++ Telegram/configure.py
@@ -65,5 +65,6 @@ if officialTarget != '':
                 arguments.append('-DTDESKTOP_API_HASH=' + apiHashMatch.group(1))
     if arch != '':
         arguments.append(arch)
-
+arguments.append('-D')
+arguments.append('CMAKE_BUILD_TYPE=Debug')
 finish(run_cmake.run(scriptName, arguments))
